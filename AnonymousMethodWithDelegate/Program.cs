0708c3c0c49a6266d6dc05fe1ed140bb366ad16f
tanmaym150 <tanmaymahamulkar150@gmail.com>
2021-08-18 10:54:00 +0530

    Add project files.

---
+++ AnonymousMethodWithDelegate/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AnonymousMethodWithDelegate
+{   //Anonymous Function
+    class Program
+    {
+        public delegate void CompanyName(string Name);
+        static void Main(string[] args)
+        {   
+            CompanyName c = delegate (string CName)
+             {
+                 Console.WriteLine("Our Company Name is: {0}", CName);
+             };
+            //user can assign this method to a variable of the delegate type
+            c("IncubXperts");
+
+                
+        }
+    }
+}
